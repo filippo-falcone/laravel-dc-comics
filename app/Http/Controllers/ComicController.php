3e8791a8dec8e83edc62149d0cb798920c8d0d87
filippo-falcone <falconefilippo98@hotmail.com>
2024-05-24 13:32:42 +0200

    feat: add edit.blade.php

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -79,7 +79,12 @@ class ComicController extends Controller
      */
     public function edit($id)
     {
-        //
+        $comic = Comic::findOrFail($id);
+        $data = [
+            'comic' => $comic
+        ];
+        
+        return view('comics.edit', $data);
     }
 
     /**
