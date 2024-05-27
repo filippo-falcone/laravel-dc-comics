9b2c0d58da718b4f5a8c999bd19d26d83374c838
filippo-falcone <falconefilippo98@hotmail.com>
2024-05-27 12:57:01 +0200

    feat: add js location function to app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,18 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+/* Window Redirect */
+const addElementBtn = document.querySelector('.add-element');
+addElementBtn.addEventListener('click', function () {
+    const comicsCreate = 'comics/create';
+    window.location.href = comicsCreate;
+});
+
+const showElement = document.querySelectorAll('.show-element');
+showElement.forEach((element, index) => {
+    element.addEventListener('click', function () {
+        const id = index + 1;
+        window.location.href = `comics/${id}`;
+    });
+});
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -13,19 +13,15 @@
                 <div class="row py-4 g-3">
                     @foreach ($comics as $comic)
                         <div class="col-6 col-md-4 col-lg-3 col-xl-2 d-flex flex-column align-items-center">
-                            <div class="img-container position-relative">
+                            <div class="show-element img-container position-relative">
                                 <img src="{{$comic->thumb}}"  alt="{{$comic->series}}">
-                                <div class="img-hover justify-content-center align-items-center position-absolute top-0 end-0 bottom-0 start-0">
-                                    <a href="{{route('comics.show', ['comic' => $comic->id])}}">Info</a>
-                                </div>
+                                <div class="img-hover justify-content-center align-items-center position-absolute top-0 end-0 bottom-0 start-0"></div>
                             </div>
                             <div class="text pt-4">{{$comic->series}}</div>
                         </div>
                     @endforeach
                 </div>
-                <button type="button" class="add-element rectangle py-2 text-center">
-                    <a href="{{route('comics.create')}}">add comic</a>
-                </button>
+                <button type="button" class="add-element rectangle py-2 text-center">add comic</button>
             </div>
         </div>
     </section>
