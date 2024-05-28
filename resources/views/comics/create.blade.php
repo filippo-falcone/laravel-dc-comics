d7379d69529f34c8e57c9e47665682f59204c869
filippo-falcone <falconefilippo98@hotmail.com>
2024-05-28 14:15:58 +0000

    feat: add @error and old function to create.blade.php

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -9,32 +9,52 @@
                 @csrf
                 <div class="mb-3">
                     <label for="title" class="form-label">Title</label>
-                    <input type="text" class="form-control" id="title" name="title">
+                    <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
                 </div>
+                @error('title')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
                 <div class="mb-3">
-                    <label for="image" class="form-label">Image</label>
-                    <input type="text" class="form-control" id="image" name="thumb">
+                    <label for="thumb" class="form-label">Image</label>
+                    <input type="text" class="form-control" id="thumb" name="thumb" value="{{ old('thumb') }}">
                 </div>
+                @error('thumb')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
                 <div class="mb-3">
                     <label for="series" class="form-label">Series</label>
-                    <input type="text" class="form-control" id="series" name="series">
+                    <input type="text" class="form-control" id="series" name="series" value="{{ old('series') }}">
                 </div>
+                @error('series')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
                 <div class="mb-3">
                     <label for="price" class="form-label">Price</label>
-                    <input type="number" step="0.01" class="form-control" id="price" name="price">
+                    <input type="number" step="0.01" class="form-control" id="price" name="price"
+                        value="{{ old('price') }}">
                 </div>
+                @error('price')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
                 <div class="mb-3">
                     <label for="date" class="form-label">Date</label>
-                    <input type="date" class="form-control" id="date" name="sale_date">
+                    <input type="date" class="form-control" id="date" name="sale_date"
+                        value="{{ old('sale_date') }}">
                 </div>
+                @error('sale_date')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
                 <select class="form-select" name="type">
-                    <option value="" selected>Select type</option>
-                    <option value="comic book">Comic Book</option>
-                    <option value="graphic novel">Graphic Novel</option>
+                    <option @selected(old('type') === '') value="" selected>Select type</option>
+                    <option @selected(old('type') === 'comic book') value="comic book">Comic Book</option>
+                    <option @selected(old('type') === 'graphic novel') value="graphic novel">Graphic Novel</option>
                 </select>
+                @error('type')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
                 <div class="mb-3">
                     <label for="description" class="form-label">Description</label>
-                    <textarea class="form-control" id="description" rows="6" name="description"></textarea>
+                    <textarea class="form-control" id="description" rows="6" name="description">{{ old('description') }}</textarea>
                 </div>
                 <button type="submit" class="rectangle py-2">Submit</button>
             </form>
